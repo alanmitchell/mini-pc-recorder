3caf43cd1c743ff98e58d166a6cbaa6cd897a880
Alan Mitchell <tabb99@gmail.com>
2025-02-21 16:32:48 -0900

    Moved location of output file check.

--- record.py
+++ record.py
@@ -14,20 +14,21 @@ sleep_time = 5.0
 
 reader_module = importlib.import_module(f"readers.{reader_module_name}")
 
-# If output file doesn't exist, create it with a header row.
+# Full path to the output file
 full_fn = Path(__file__).parent / "data" / fn
-if not full_fn.exists():
-    with open(full_fn, 'w') as fout:
-        fout.write(f"{reader_module.header_row()}\n")
 
 while True:
+    # If output file doesn't exist, create it with a header row.
+    if not full_fn.exists():
+        with open(full_fn, 'w') as fout:
+            fout.write(f"{reader_module.header_row()}\n")
     try:
         data_line = reader_module.read_data()
         if data_line:
             with open(full_fn, 'a') as fout:
                 fout.write(f"{data_line}\n")
-        time.sleep(sleep_time)
 
     except Exception as e:
         print(e)
-        time.sleep(sleep_time)
+
+    time.sleep(sleep_time)
